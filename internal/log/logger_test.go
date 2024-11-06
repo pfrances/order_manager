3b4a8891abab02deaeb8cb677ac584cf3a425af3
Franceschi Pierre <franceschi.pierre@hotmail.fr>
2024-11-06 10:02:06 +0900

    add test on logger

--- internal/log/logger_test.go
+++ internal/log/logger_test.go
@@ -28,3 +28,66 @@ func TestDebugLogger(t *testing.T) {
 	assert.Contains(t, errBuff.String(), "warning")
 	assert.Contains(t, errBuff.String(), "error")
 }
+
+func TestInfoLogger(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuff := new(bytes.Buffer)
+	logger := log.New(log.Info, buf, errBuff)
+
+	logger.Debugf("debug")
+	logger.Infof("info")
+	logger.Warningf("warning")
+	logger.Errorf("error")
+
+	assert.NotContains(t, buf.String(), "debug")
+	assert.Contains(t, buf.String(), "info")
+	assert.NotContains(t, buf.String(), "warning")
+	assert.NotContains(t, buf.String(), "error")
+
+	assert.NotContains(t, errBuff.String(), "debug")
+	assert.NotContains(t, errBuff.String(), "info")
+	assert.Contains(t, errBuff.String(), "warning")
+	assert.Contains(t, errBuff.String(), "error")
+}
+
+func TestWarningLogger(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuff := new(bytes.Buffer)
+	logger := log.New(log.Warning, buf, errBuff)
+
+	logger.Debugf("debug")
+	logger.Infof("info")
+	logger.Warningf("warning")
+	logger.Errorf("error")
+
+	assert.NotContains(t, buf.String(), "debug")
+	assert.NotContains(t, buf.String(), "info")
+	assert.NotContains(t, buf.String(), "warning")
+	assert.NotContains(t, buf.String(), "error")
+
+	assert.NotContains(t, errBuff.String(), "debug")
+	assert.NotContains(t, errBuff.String(), "info")
+	assert.Contains(t, errBuff.String(), "warning")
+	assert.Contains(t, errBuff.String(), "error")
+}
+
+func TestErrorLogger(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuff := new(bytes.Buffer)
+	logger := log.New(log.Error, buf, errBuff)
+
+	logger.Debugf("debug")
+	logger.Infof("info")
+	logger.Warningf("warning")
+	logger.Errorf("error")
+
+	assert.NotContains(t, buf.String(), "debug")
+	assert.NotContains(t, buf.String(), "info")
+	assert.NotContains(t, buf.String(), "warning")
+	assert.NotContains(t, buf.String(), "error")
+
+	assert.NotContains(t, errBuff.String(), "debug")
+	assert.NotContains(t, errBuff.String(), "info")
+	assert.NotContains(t, errBuff.String(), "warning")
+	assert.Contains(t, errBuff.String(), "error")
+}
